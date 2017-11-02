2db103278858d0a5347e9a9c4ed7ef5b6bc69aaf
hillcd2 <33285035+hillcd2@users.noreply.github.com>
2017-11-02 08:42:28 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,41 @@
+﻿use hawks
+
+GO 
+CREATE TABLE Project
+(
+	projectID int NOT NULL,
+	projectName VARCHAR(20),
+	major VARCHAR(20),
+	GPA int, 
+	size int,
+	descr VARCHAR(20),
+	primary key (projectID)
+);
+
+GO
+CREATE TABLE Student  
+(
+	studentID int NOT NULL,
+	major VARCHAR(20),
+	GPA int, 
+	interests VARCHAR(20),
+	primary key (studentID)
+);
+
+GO
+CREATE TABLE Advisor 
+(
+	advisorID int NOT NULL,
+	advisorName VARCHAR(20),
+	primary key (advisorID)
+);
+
+GO CREATE TABLE Time_Slot(
+	schedtime int
+);
+
+GO CREATE TABLE Student_In_Time_Slot(
+	schedtime int
+	--
+);
+
